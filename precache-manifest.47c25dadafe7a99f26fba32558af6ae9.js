a081796e29950193c393c29d7e1c83e83acddea0
suryakantgupta <suryakantgupta66@gmail.com>
2021-02-19 15:28:04 +0530

    Updates

---
+++ precache-manifest.47c25dadafe7a99f26fba32558af6ae9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6983c9aaf3e4fdcaf180812199fab7e",
+    "url": "/neostore/index.html"
+  },
+  {
+    "revision": "bf72fb978df20a51f912",
+    "url": "/neostore/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "2fda64dfe1397dfce2bf",
+    "url": "/neostore/static/css/main.2f7869c0.chunk.css"
+  },
+  {
+    "revision": "bf72fb978df20a51f912",
+    "url": "/neostore/static/js/2.545f8652.chunk.js"
+  },
+  {
+    "revision": "1f150e7a2ecd474fa0221a057c7987e9",
+    "url": "/neostore/static/js/2.545f8652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fda64dfe1397dfce2bf",
+    "url": "/neostore/static/js/main.5fae2872.chunk.js"
+  },
+  {
+    "revision": "9d778664b793f633c484",
+    "url": "/neostore/static/js/runtime-main.cd45a5c4.js"
+  },
+  {
+    "revision": "310aacb6b16a1d50f542192c4151ce9f",
+    "url": "/neostore/static/media/Goku.310aacb6.png"
+  },
+  {
+    "revision": "625b944a49b58c1008c8060fc6cb51ec",
+    "url": "/neostore/static/media/cake.625b944a.jpg"
+  }
+]);
